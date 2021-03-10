cfb0fa425872992dc510c34b5ae3ea4339dc9494
Cory Kelley <coryk0789@gmail.com>
2021-03-09 19:42:10 -0500

    updated index page, added sticky header func

--- components/Layout.js
+++ components/Layout.js
@@ -1,31 +1,53 @@
+import styled from '@emotion/styled';
+
 const Layout = ({ children }) => {
 	return (
-		<div
-			style={{ maxWidth: '42rem', margin: '0 auto', padding: '1rem 1.7rem' }}
-		>
-			<header
-				style={{
-					display: 'flex',
-					flexDirection: 'row',
-					justifyContent: 'space-between',
-					position: 'sticky',
-					zIndex: '10',
-					top: 0,
-					backdropFilter: 'saturate(180%) blur(20px)',
-				}}
-			>
+		<StyledLayoutContainer>
+			<StyledHeader>
 				<h3>logo</h3>
 				<nav>
-					<ul style={{ display: 'flex', flexDirection: 'row' }}>
-						<li>something</li>
-						<li>something</li>
-						<li>something</li>
+					<ul>
+						<li>About</li>
+						<li>Blog</li>
+						<li>Projects</li>
 					</ul>
 				</nav>
-			</header>
+			</StyledHeader>
 			<main>{children}</main>
-		</div>
+			<footer>this is the footer</footer>
+		</StyledLayoutContainer>
 	);
 };
 
 export default Layout;
+
+const StyledLayoutContainer = styled.div`
+	max-width: 41rem;
+	margin: 0 auto;
+	padding: 1rem 1.7rem;
+
+	main {
+		min-height: 85vh;
+	}
+`;
+
+const StyledHeader = styled.header`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	position: sticky;
+	z-index: 10;
+	top: 0;
+	margin: 2rem 0 2rem;
+	background-color: #f1f3f5;
+	padding: 1.5rem 0;
+
+	ul {
+		display: flex;
+		flex-direction: row;
+
+		li {
+			padding-left: 1.75rem;
+		}
+	}
+`;
--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,101 @@ import Layout from '../components/Layout';
 import { getPosts } from '../lib/posts';
 import Link from 'next/link';
 
+import styled from '@emotion/styled';
+
 const Index = ({ posts }) => {
 	console.log(posts);
 	return (
 		<Layout>
-			<ul>
-				{posts.map((post) => (
-					<li key={post.id}>
-						<Link href={`/posts/${post.slug}`}>
-							<a>{post.title}</a>
-						</Link>
-					</li>
-				))}
-			</ul>
+			<StyledMainContent>
+				<section className='intro'>
+					<h1>Hey, I'm Cory.</h1>
+					<p>
+						I'm a software engineer, web developer, writer, and occasional video
+						creator. Depending on the season, I'm most likely watching college
+						football.
+					</p>
+				</section>
+				<section className='latest-blog'>
+					<h2>Latest Posts</h2>
+					<ul>
+						{posts.map((post) => (
+							<li key={post.id}>
+								<h3>
+									<Link href={`/posts/${post.slug}`}>{post.title}</Link>
+								</h3>
+								<p>{post.excerpt}</p>
+							</li>
+						))}
+					</ul>
+					<ul>
+						{posts.map((post) => (
+							<li key={post.id}>
+								<h3>
+									<Link href={`/posts/${post.slug}`}>{post.title}</Link>
+								</h3>
+								<p>{post.excerpt}</p>
+							</li>
+						))}
+					</ul>
+					<ul>
+						{posts.map((post) => (
+							<li key={post.id}>
+								<h3>
+									<Link href={`/posts/${post.slug}`}>{post.title}</Link>
+								</h3>
+								<p>{post.excerpt}</p>
+							</li>
+						))}
+					</ul>
+					<ul>
+						{posts.map((post) => (
+							<li key={post.id}>
+								<h3>
+									<Link href={`/posts/${post.slug}`}>{post.title}</Link>
+								</h3>
+								<p>{post.excerpt}</p>
+							</li>
+						))}
+					</ul>
+					<ul>
+						{posts.map((post) => (
+							<li key={post.id}>
+								<h3>
+									<Link href={`/posts/${post.slug}`}>{post.title}</Link>
+								</h3>
+								<p>{post.excerpt}</p>
+							</li>
+						))}
+					</ul>
+				</section>
+			</StyledMainContent>
 		</Layout>
 	);
 };
 
 export default Index;
+
+const StyledMainContent = styled.div`
+	.intro {
+		margin: 5rem 0;
+
+		h1 {
+			font-size: 2.75rem;
+			margin-bottom: 1.3rem;
+		}
+	}
+
+	.latest-blog {
+		h2 {
+			margin-bottom: 0.9rem;
+		}
+
+		li {
+			margin-bottom: 0.9rem;
+		}
+	}
+`;
 
 export async function getStaticProps(context) {
 	const posts = await getPosts();
